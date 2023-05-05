4edc160ab455b543e3b37d8a0d8900ea01679126
Anjali Singh <105203288+Anjali0048@users.noreply.github.com>
2023-05-05 16:36:29 +0530

    commit all

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-blue-700 text-3xl'>Anjali Singh</h1>
+    </div>
+  );
+}
+
+export default App;
